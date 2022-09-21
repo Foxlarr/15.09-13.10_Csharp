5a73daafed8f4f73dd64294b24b171753b402c94
Foxxlarr <talismafeed@yandex.ru>
2022-09-21 14:15:09 +0300

    Some code added

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0 Демонстрация решения
+// Напишите программу, которая
+// 1. На вход принимает число и
+// 2. выдаёт его квадрат (число, умноженное само на себя)
+// Вывод результата
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+
+Console.Write("input a number you want to exponate: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int squear = number * number;
+
+Console.WriteLine($"{number}^2 = {squear}");
+
+// Math.Pow(number, 2);
