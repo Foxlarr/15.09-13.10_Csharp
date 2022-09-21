c0c1ca987273cc73a8d5907fb420dd5c0cc1a12f
Foxxlarr <talismafeed@yandex.ru>
2022-09-21 16:33:03 +0300

    Did some sketchy **** not on seminar

---
+++ Task00,5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Wanna know if the first (a) number is a result of second one (b) rased to the power of 2?");
+Console.WriteLine("Type first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type second number");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b*b == a)
+{
+    Console.WriteLine($"Woah! You guessed it. Somehow. {a} is actually {b}^2");
+}
+else
+{
+    Console.WriteLine($"Nah,{a} isn't the result of {b}^2");
+}
