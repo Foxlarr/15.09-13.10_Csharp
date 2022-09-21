5684bf01689932a8ecd66bcf54a2a6d0e0d51aa9
Foxxlarr <talismafeed@yandex.ru>
2022-09-21 16:18:28 +0300

    Coded some code

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Type number of day (1 to 7)");
+int a = Convert.ToInt32(Console.ReadLine());
+string[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+if (a>7)
+{
+    Console.WriteLine("There are only 7(seven) days, you dumb coconut. Try again.");
+}
+else
+{
+Console.WriteLine(weekDays[a]);
+}
+
