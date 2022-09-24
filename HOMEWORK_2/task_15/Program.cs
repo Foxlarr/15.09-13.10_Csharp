5b6847f6a3890bd0056abd6b522730fe2cb22ded
Foxxlarr <talismafeed@yandex.ru>
2022-09-24 19:38:09 +0300

    task 15

---
+++ HOMEWORK_2/task_15/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Type number of day (1 to 7), to to find out if it's a weekend or not.");
+string a = Console.ReadLine();
+switch (a)
+{
+    case "1":
+        Console.WriteLine("no");
+        break;
+    case "2":
+        Console.WriteLine("no");
+        break;
+    case "3":
+        Console.WriteLine("no");
+        break;
+    case "4":
+        Console.WriteLine("no");
+        break;
+    case "5":
+        Console.WriteLine("no");
+        break;
+    case "6":
+        Console.WriteLine("yes");
+        break;
+    case "7":
+        Console.WriteLine("yes");
+        break;
+    default:
+    Console.WriteLine("Your input was not the numbers from 1 to 7.");
+    break;
+}
