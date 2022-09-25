34b1865480f789af63943e3ba70256970e4de282
Foxxlarr <talismafeed@yandex.ru>
2022-09-25 12:05:53 +0300

    task 13 and 10

--- HOMEWORK_2/task_10/Program.cs
+++ HOMEWORK_2/task_10/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// FIRST ONE
+Console.WriteLine("Input random number to see the second digit of it");
+int a = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+if (a < 0)
+{
+  a = -a ;
+}
+while (a > 100)
+{
+  a = (a / 10);
+}
+n = (a % 10);
+Console.WriteLine (n);
+
+// SECOND ONE
+
+// Console.WriteLine("Input random number to see the second digit of it");
+// string input = Console.ReadLine();
+// Console.WriteLine("{0}->{1}",input, input[1]);
--- HOMEWORK_2/task_13/Program.cs
+++ HOMEWORK_2/task_13/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input random number to see the third digit of it");
+string input = Console.ReadLine();
+if (input.Length < 3)
+{
+    Console.WriteLine("There is no 3rd digit");
+}
+else
+{
+Console.WriteLine("{0}->{1}",input, input[2]);
+}
