24717ce8ab461aa479450c7ada34cc3b9bb2e768
Foxxlarr <talismafeed@yandex.ru>
2022-10-08 21:06:19 +0300

    forgotten hw

---
+++ HOMEWORK_6/task_41/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Input nums (use space to sepparate): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Count of nums > 0 -> {count}");
---
+++ HOMEWORK_6/task_43/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+string FindTheSpot(double k1, double b1, double k2, double b2)
+{
+    double x = (b2-b1)/(k1-k2);
+    double y = k1*((b2 - b1)/(k1 - k2))+b1;
+    return ($"{x},{y}");
+}
+string y = FindTheSpot(k1, b1, k2, b2);
+System.Console.WriteLine($"intersection point coordinates are {y}");
