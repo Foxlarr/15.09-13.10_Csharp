12245eb8558f2a98de0be2c304785d917a48b0b6
Foxxlarr <talismafeed@yandex.ru>
2022-10-18 23:07:15 +0300

    sad final

---
+++ HOMEWORK_9/task64/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void AllNatural(int num)
+{
+    System.Console.Write($"{num} ");
+    if (num != 1) AllNatural(num - 1);
+}
+
+System.Console.WriteLine("input n");
+int num = Convert.ToInt32(Console.ReadLine());
+AllNatural(num);
---
+++ HOMEWORK_9/task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int AllNatural(int m, int n)
+{
+    int res = n;
+    
+    if (m != n) res = n + AllNatural(m , n - 1);
+    System.Console.WriteLine($"m {m}, n {n}, res {res}"); 
+  
+    return res;
+}
+
+System.Console.WriteLine("input m");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("input n");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m>n) (m, n) = (n, m);
+int res = AllNatural(m,n);
+System.Console.WriteLine(res);
